07b9930a9e3a04b9afb42b6f7fbd36d49fb44576
lishujing <super_lishujing@163.com>
2022-03-23 17:12:10 +0800

    feat:新增商品列表展示

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import 'vant/lib/button/style';
-import { Cell, NavBar, Search, Icon } from 'vant';
+import { Cell, NavBar, Search, Icon, Rate } from 'vant';
 
 Vue.use(Cell);
 Vue.use(NavBar);
 Vue.use(Search);
 Vue.use(Icon);
+Vue.use(Rate);
--- src/views/msite/index.vue
+++ src/views/msite/index.vue
@@ -8,19 +8,63 @@
         <span>登录 | 注册</span>
       </template>
     </van-nav-bar>
+
+    <nav class="msite_nav">
+      <div class="swiper-container" v-if="foodTypes.length">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide food_types_container" v-for="(item, index) in foodTypes" :key="index">
+            <router-link
+              :to="{
+                path: '/food',
+                query: {
+                  geohash,
+                  title: foodItem.title,
+                  restaurant_category_id: getCategoryId(foodItem.link),
+                },
+              }"
+              v-for="foodItem in item"
+              :key="foodItem.id"
+              class="link_to_food"
+            >
+              <figure>
+                <img :src="imgBaseUrl + foodItem.image_url" />
+                <figcaption>{{ foodItem.title }}</figcaption>
+              </figure>
+            </router-link>
+          </div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+      <img src="../../assets/images/fl.svg" class="fl_back animation_opactiy" v-else />
+    </nav>
+
+    <div class="shop_list_container">
+      <header class="shop_header">
+        <van-icon name="shop-o" />
+        <span class="shop_header_title">附近商家</span>
+      </header>
+      <shop-list v-if="hasGetData" :geohash="geohash"></shop-list>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
-import { cityGuess, msiteAddress } from '@/api/getData'
+import { mapMutations } from 'vuex'
+import shopList from "@/components/common/shoplist";
+import { cityGuess, msiteAddress, msiteFoodTypes } from '@/api/getData'
+import "@/plugins/swiper.min.js";
+import "@/assets/style/swiper.min.css";
 export default {
+  components:{
+    shopList
+  },
   data() {
     return {
       geohash: '779', // city页面传递过来的地址geohash
       msiteTitle: '请选择地址...', // msite页面头部标题
       foodTypes: [], // 食品分类列表
-      hasGetData:false, // 是否已经获取地理位置数据,成功之后再获取商铺列表信息
+      hasGetData: false, // 是否已经获取地理位置数据,成功之后再获取商铺列表信息
+      imgBaseUrl: 'https://fuss10.elemecdn.com', //图片域名地址
     }
   },
   async beforeMount() {
@@ -31,19 +75,49 @@ export default {
       this.geohash = this.$route.query.geohash
     }
     // 保存 geohash 到 vuex
-    this.SAVE_GEOHASH(this.geohash);
+    this.SAVE_GEOHASH(this.geohash)
     // 获取当前位置信息
     let res = await msiteAddress(this.geohash)
     this.msiteTitle = res.name
     // 记录当前经纬度
-    this.RECORD_ADDRESS(res);
-    this.hasGetData = true;
+    this.RECORD_ADDRESS(res)
+    this.hasGetData = true
+  },
+  mounted() {
+    // 获取导航食品类型列表
+    msiteFoodTypes(this.geohash).then((res) => {
+      let resLength = res.length
+      let resArr = [...res] // 返回一个新的数组
+      let footArr = []
+      for (let i = 0, j = 0; i < resLength; i += 8, j++) {
+        footArr[j] = resArr.splice(0, 8)
+      }
+      this.foodTypes = footArr
+    }).then(()=>{
+      // 初始化swiper
+      new Swiper(".swiper-container",{
+        pagination:".swiper-pagination",
+        loop:true
+      });
+    });
   },
   methods: {
-    ...mapMutations(["RECORD_ADDRESS", "SAVE_GEOHASH"]),
+    ...mapMutations(['RECORD_ADDRESS', 'SAVE_GEOHASH']),
+    // 点击搜索icon
     toSearch() {
       this.$router.push({ path: '/search/geohash' })
     },
+    // 解吗url地址,求去restaurant_category_id的值
+    getCategoryId(url){
+      let urlData = decodeURIComponent(
+        url.split("=")[1].replace("&target_name", "")
+      )
+      if (/restaurant_category_id/gi.test(urlData)) {
+        return JSON.parse(urlData).restaurant_category_id.id;
+      } else {
+        return "";
+      }
+    }
   },
 }
 </script>
@@ -68,10 +142,10 @@ export default {
   }
 }
 .msite_nav {
-  padding-top: 2.1rem;
+  padding-top: 0.3rem;
   background-color: #fff;
   border-bottom: 0.025rem solid $bc;
-  height: 10.6rem;
+  height: 9rem;
   .swiper-container {
     @include wh(100%, auto);
     padding-bottom: 0.6rem;
@@ -107,6 +181,7 @@ export default {
   border-top: 0.025rem solid $bc;
   background-color: #fff;
   .shop_header {
+    padding: 0.3rem 0 0 0.6rem;
     .shop_icon {
       fill: #999;
       margin-left: 0.6rem;
